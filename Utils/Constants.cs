796cda9c86e200d68f0db7beb6bc557a31485172
ifBars <tristens5465@gmail.com>
2026-07-19 21:04:44 -0700

    release: Bars Graphics 1.0.2

--- Utils/Constants.cs
+++ Utils/Constants.cs
@@ -4,7 +4,7 @@
     {
         public const string ModId = "BarsGraphics";
         public const string ModName = "Bars Graphics";
-        public const string ModVersion = "1.0.1";
+        public const string ModVersion = "1.0.2";
         public const string ModAuthor = "Bars";
         public const string PreferencesCategory = ModId;
         public const string GameStudio = "TVGS";
